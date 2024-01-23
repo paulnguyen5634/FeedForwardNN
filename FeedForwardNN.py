49e1becca3499e6d6ff8a18768ede059b93f0157
paulnguyen5634 <paulnguyen@ufl.edu>
2024-01-22 21:30:26 -0500

    load dataset

---
+++ FeedForwardNN.py
@@ -0,0 +1,40 @@
+'''
+MNIST
+'''
+
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+# device config
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# hyper parameters
+input_size = 784 # 28x28
+hidden_size = 100 # You can try different sizes later
+num_classes = 10 # ten different digits
+num_epochs = 2 # Can be more later for better training
+batch_size =100
+learning_rate = 0.001
+
+# Downloading the MNIST dataset
+train_dataset = torchvision.datasets.MNIST(root='./data', 
+                                           train=True, 
+                                           transform=transforms.ToTensor(),  
+                                           download=True)
+
+test_dataset = torchvision.datasets.MNIST(root='./data', 
+                                          train=False, 
+                                          transform=transforms.ToTensor())
+
+# Data loader
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                           batch_size=batch_size, 
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
+                                          batch_size=batch_size, 
+                                          shuffle=False)
+
