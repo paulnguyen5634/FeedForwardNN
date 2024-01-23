ca1db1b87517a7a1e3d78426821c8d2477d3870a
paulnguyen5634 <paulnguyen@ufl.edu>
2024-01-22 21:46:50 -0500

    Created general layout of NN

--- FeedForwardNN.py
+++ FeedForwardNN.py
@@ -52,3 +52,18 @@ for i in range(6):
     plt.imshow(example_data[i][0], cmap='gray')
 plt.show()
 
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.l1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.l2 = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        out = self.l1(x)
+        out = self.relu(out)
+        out = self.l2(out) # Since its a multiclass classification, we dont want a softmax here. Will find the loss outside
+        return out
+    
+model = NeuralNet(input_size, hidden_size, num_classes).to(device)
+
