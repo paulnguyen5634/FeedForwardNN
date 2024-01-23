144ea50b11748ea6daf2f933aadaf588d717f077
paulnguyen5634 <paulnguyen@ufl.edu>
2024-01-22 21:36:12 -0500

    added visual examples of mnist data

--- FeedForwardNN.py
+++ FeedForwardNN.py
@@ -38,3 +38,17 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           batch_size=batch_size, 
                                           shuffle=False)
 
+examples = iter(test_loader)
+example_data, example_targets = next(examples)
+print(example_data.shape, example_targets.shape) 
+
+'''
+torch.Size([100, 1, 28, 28]): We have 100 samples, 1 color channel, 28x28 
+torch.Size([100]): For each of the 100 samples we have one output
+'''
+
+for i in range(6):
+    plt.subplot(2,3,i+1)
+    plt.imshow(example_data[i][0], cmap='gray')
+plt.show()
+
